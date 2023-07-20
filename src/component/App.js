0ec5cd6e20377da36383527df5f316065fb8cf00
Evghenii Dolomanji <evgheniidolomanji@EN411673.local>
2023-07-20 17:55:33 +0300

    initial commit

---
+++ src/component/App.js
@@ -0,0 +1,21 @@
+import '../index.css'
+function App() {
+  return (
+      <div className="h-full border-2 border-gray-200
+                border-opacity-60 rounded-lg
+                overflow-hidden">
+
+          <div className="p-6 hover:bg-green-600
+                    hover:text-white transition
+                    duration-300 ease-in">
+
+              <h1 className="text-2xl font-semibold mb-3">
+                  Hover
+              </h1>
+          </div>
+      </div>
+
+);
+}
+
+export default App;
