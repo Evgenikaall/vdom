bb16dd66d0b62387fc9c24263783190a90631eae
Evghenii Dolomanji <evgheniidolomanji@en411673.local>
2023-07-20 18:34:27 +0300

    Implement navbar feature

--- src/component/App.js
+++ src/component/App.js
@@ -1,20 +1,8 @@
 import '../index.css'
+import Navbar from "./Navbar";
 function App() {
   return (
-      <div className="h-full border-2 border-gray-200
-                border-opacity-60 rounded-lg
-                overflow-hidden">
-
-          <div className="p-6 hover:bg-green-600
-                    hover:text-white transition
-                    duration-300 ease-in">
-
-              <h1 className="text-2xl font-semibold mb-3">
-                  Hover
-              </h1>
-          </div>
-      </div>
-
+    <Navbar/>
 );
 }
 
