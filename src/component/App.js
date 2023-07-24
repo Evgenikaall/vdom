a8af1ddc46320bfce9b69d69dad9892534467f99
Evgenikaall <59172058+Evgenikaall@users.noreply.github.com>
2023-07-24 16:10:43 +0300

    workflow base (#5)

--- src/component/App.js
+++ src/component/App.js
@@ -2,12 +2,14 @@ import '../index.css'
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import WorkingArea from "./workingArea/WorkingArea";
+import Workflow from "./workflow/Workflow";
 
 function App() {
   return (
       <React.Fragment>
         <Navbar />
         <WorkingArea />
+        <Workflow/>
       </React.Fragment>
   );
 }
