16293be277693140c66e52ffcc7b2b7876053b5c
evgheniidolomanji <evgenikaall@gmail.com>
2023-07-31 01:19:54 +0300

    explore & footer (prev code adjustments)

--- src/component/App.js
+++ src/component/App.js
@@ -3,15 +3,19 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import WorkingArea from "./workingArea/WorkingArea";
 import Workflow from "./workflow/Workflow";
+import Explore from "./explore/Explore";
+import Footer from "./footer/Footer";
 
 function App() {
-  return (
-      <React.Fragment>
-        <Navbar />
-        <WorkingArea />
-        <Workflow/>
-      </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Navbar/>
+            <WorkingArea/>
+            <Explore/>
+            <Workflow/>
+            <Footer/>
+        </React.Fragment>
+    );
 }
 
 export default App;
--- src/component/workflow/Workflow.js
+++ src/component/workflow/Workflow.js
@@ -4,29 +4,33 @@ import {faCampground} from "@fortawesome/free-solid-svg-icons";
 
 function Workflow() {
     return (
-        <section className="w-full h-50vh flex justify-around items-center flex-col mt-10">
-            <h1 className="text-6xl mb">Workflow</h1>
-            <div className="w-11/12 h-30vh flex px-10 justify-around ">
-                <div className="w-full absolute left-1/2 right-1/2 -translate-x-1/2 translate-y-1/2 h-15vh border-2 border-gray-500 rounded-t-4.5rem rounded-b-4.5rem"></div>
-                <div className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+        <section className="w-full h-50vh flex justify-around items-center flex-col">
+            <h1 className="text-6xl mb-7">Workflow</h1>
+            <div className="w-full h-[35vh] flex px-10 justify-around ">
+                <div className="w-[97%] absolute left-1/2 right-1/2 -translate-x-1/2 translate-y-1/2 h-15vh border-2 border-gray-500 rounded-t-4.5rem rounded-b-4.5rem"></div>
+                <div
+                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
-                    <h1 className="text-2xl font-bold pb-3">text</h1>
-                    <p>fndasfnadsj fasdjlfn ajlsdnf jlasdf jlasdfn jadsfn kadsnf kadskasdkf</p>
+                    <h1 className="text-2xl font-bold pb-3">Ordering</h1>
+                    <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
-                <div className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                <div
+                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
-                    <h1 className="text-2xl font-bold pb-3">text</h1>
-                    <p>fndasfnadsj fasdjlfn ajlsdnf jlasdf jlasdfn jadsfn kadsnf kadskasdkf</p>
+                    <h1 className="text-2xl font-bold pb-3">Ordering</h1>
+                    <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
-                <div className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                <div
+                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
-                    <h1 className="text-2xl font-bold pb-3">text</h1>
-                    <p>fndasfnadsj fasdjlfn ajlsdnf jlasdf jlasdfn jadsfn kadsnf kadskasdkf</p>
+                    <h1 className="text-2xl font-bold pb-3">Ordering</h1>
+                    <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
-                <div className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                <div
+                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
-                    <h1 className="text-2xl font-bold pb-3">text</h1>
-                    <p>fndasfnadsj fasdjlfn ajlsdnf jlasdf jlasdfn jadsfn kadsnf kadskasdkf</p>
+                    <h1 className="text-2xl font-bold pb-3">Ordering</h1>
+                    <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
             </div>
 
