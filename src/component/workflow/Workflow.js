994c0813cdfcec995d1ec6b7a422c5a6f24f2d4e
evgheniidolomanji <evgenikaall@gmail.com>
2023-08-04 14:29:27 +0300

    adjust responsibility

--- src/component/workflow/Workflow.js
+++ src/component/workflow/Workflow.js
@@ -4,30 +4,29 @@ import {faCampground} from "@fortawesome/free-solid-svg-icons";
 
 function Workflow() {
     return (
-        <section className="w-full h-50vh flex justify-around items-center flex-col">
-            <h1 className="text-6xl mb-7">Workflow</h1>
-            <div className="w-full h-[35vh] flex px-10 justify-around ">
-                <div className="w-[97%] absolute left-1/2 right-1/2 -translate-x-1/2 translate-y-1/2 h-15vh border-2 border-gray-500 rounded-t-4.5rem rounded-b-4.5rem"></div>
-                <div
-                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+        <section className="w-full flex justify-around items-center flex-col pt-10">
+            <h1 className="text-6xl mb-10">Workflow</h1>
+            <div className="w-full flex px-10 justify-around max-lg:flex-col max-lg:items-center ">
+                <div className="w-[97%] absolute left-1/2 right-1/2 -translate-x-1/2 translate-y-1/2 h-15vh border-2 border-gray-500 rounded-t-4.5rem rounded-b-4.5rem max-lg:hidden"></div>
+                <div className="flex-col w-[20%] max-lg:w-[50%] mb-5 flex bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
                     <h1 className="text-2xl font-bold pb-3">Ordering</h1>
                     <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
                 <div
-                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                    className="flex-col w-[20%] max-lg:w-[50%] mb-5 flex bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
                     <h1 className="text-2xl font-bold pb-3">Ordering</h1>
                     <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
                 <div
-                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                    className="flex-col w-[20%] max-lg:w-[50%] mb-5 flex bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
                     <h1 className="text-2xl font-bold pb-3">Ordering</h1>
                     <p>Includes construction selecting, free consultation, extra fees</p>
                 </div>
                 <div
-                    className="flex-col flex basis-1/6 bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
+                    className="flex-col w-[20%] max-lg:w-[50%] mb-5 flex bg-main-color z-10 items-start px-5 py-7 rounded-t-2rem rounded-b-2rem">
                     <FontAwesomeIcon icon={faCampground} className="text-red-600 self-center pb-5" size="2xl"/>
                     <h1 className="text-2xl font-bold pb-3">Ordering</h1>
                     <p>Includes construction selecting, free consultation, extra fees</p>
