13ef53c018640a52c33acdd7d6c52afbef59567f
Evgenikaall <59172058+Evgenikaall@users.noreply.github.com>
2023-08-14 14:46:38 +0300

    preloader (#14)

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,5 @@
 import '../index.css'
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "./navbar/Navbar";
 import WorkingArea from "./workingArea/WorkingArea";
 import Intro from "./Intro/Intro";
@@ -9,21 +9,36 @@ import Explore from "./explore/Explore";
 import Footer from "./footer/Footer";
 import Delivery from './delivery/Delivery';
 import QuestionList from "./question/QuestionList";
+import Preloader from "./preloader/Preloader";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate an async operation, like fetching data
+        setTimeout(() => {
+            setLoading(false);
+        }, 1600);
+    }, []);
+
     return (
-        <React.Fragment>
-            <Navbar/>
-            <Intro/>
-            <Info/>
-            <WorkingArea/>
-            <Explore/>
-            <Workflow/>
-            <Delivery/>
-            <QuestionList/>
-            <Footer/>
-        </React.Fragment>
-    );
+        <div className="min-h-screen flex justify-center items-center">
+            <div className={`transition-opacity duration-[1800ms] ${loading ? 'opacity-0' : 'opacity-100'}`}>
+                <Navbar/>
+                <Intro/>
+                <Info/>
+                <WorkingArea/>
+                <Explore/>
+                <Workflow/>
+                <Delivery/>
+                <QuestionList/>
+                <Footer/>
+            </div>
+            <div className={`transition-opacity duration-[1800ms] ${loading ? 'opacity-100' : 'opacity-0 hidden'}`}>
+                <Preloader/>
+            </div>
+        </div>
+    )
 }
 
 export default App;
